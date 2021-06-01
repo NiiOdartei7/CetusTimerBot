64a79a7028c3f3d0a0a112160574f3aeccfbb681
NiiOdartei7 <43499683+NiiOdartei7@users.noreply.github.com>
2021-06-01 12:15:44 +0000

    Update cetustimerbot.py

--- cetustimerbot.py
+++ cetustimerbot.py
@@ -17,7 +17,7 @@ bot.
 
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import requests
+
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
