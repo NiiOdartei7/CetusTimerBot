7e1ddce9eae9acdfcf1de0f9aa8185ccc02a98af
NiiOdartei7 <43499683+NiiOdartei7@users.noreply.github.com>
2021-06-18 19:01:38 +0000

    Update cetustimerbot.py

--- cetustimerbot.py
+++ cetustimerbot.py
@@ -1,19 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# This program is dedicated to the public domain under the CC0 license.
 
-"""
-Simple Bot to reply to Telegram messages.
 
-First, a few handler functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
 
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
